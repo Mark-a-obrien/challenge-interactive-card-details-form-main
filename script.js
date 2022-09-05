04381f25d7b084806580e3cc55e0d72da4149986
Mark-a-obrien <mark.a.obrien00@gmail.com>
2022-09-05 18:53:00 +0100

    complete state section created

--- script.js
+++ script.js
@@ -29,102 +29,138 @@ function displayError(value, length, text, maxLength) {
 }
 
 
+let numberError = false;
+let nameError = false;
+let dateError = false;
+let letCvc = false;
+
+function errorCheckAndInsert(e, textfield) {
+
+
+    if (textfield.id === "name" && textfield.value.length < 23) {
+        cardName.textContent = textfield.value.toUpperCase();
+
+        if (textfield.value.length === 0) {
+            cardName.textContent = "JANE APPLESEED";
+        }
+    } 
+        
+
+    // // updates card number on the front of the card
+    else if (textfield.id === "number") {
+
+        let errorText = displayError(textfield.value,textfield.value.length, numberErrorMsg.textContent, 20);
+        if (errorText != "") {
+            numberErrorMsg.textContent = errorText;
+            numberError = true;
+            return true;
+        } else {
+            numberError = false;
+            numberErrorMsg.textContent = "";
+        }
+
+
+        if (textfield.value.length === 0) {
+            cardNum.textContent = "0000 0000 0000 0000"; // resets the card number
+        } 
+        else if (textfield.value.length === 20){ 
+            cardNum.textContent = textfield.value;
+        }
+        else {
+            cardNum.textContent = textfield.value;
+        }
+
+        if (e.inputType != "deleteContentBackward") {
+            if (textfield.value.length === 4 || textfield.value.length === 9 || textfield.value.length === 14) {
+                textfield.value += " "; 
+            };
+        }
+    }
+
+    // updates moneth & year on the front of the card
+    if (textfield.id === "month" || textfield.id === "year") {
+
+        let errorText = displayError(textfield.value,textfield.value.length, monthAndYearErrorMsg.textContent, 3);
+        if (errorText != "") {
+            monthAndYearErrorMsg.textContent = errorText;
+            dateError = true;
+            return true;
+        } else {
+            dateError = false;
+            monthAndYearErrorMsg.textContent = "";
+        }
+
+
+        if (textfield.id === "month") {
+            if (textfield.value.length <= 2) {
+                cardMonth.textContent = textfield.value;
+            }
+
+            if (textfield.value.length === 0) {
+                cardMonth.textContent = "00";
+            }
+        }
+
+        if (textfield.id === "year") {
+            if (textfield.value.length <= 2) {
+                cardYear.textContent = textfield.value;
+            } 
+
+            if (textfield.value.length === 0) {
+                cardYear.textContent = "00";
+            }
+        }
+    }
+
+    // updates cvc number on the back of the card
+    if (textfield.id === "cvc") {
+
+        let errorText = displayError(textfield.value, textfield.value.length, cvcErrorMsg.textContent, 4);
+        if (errorText != "") {
+            cvcErrorMsg.textContent = errorText;
+            letCvc = true;
+            return true;
+        } else {
+            letCvc = false;
+            cvcErrorMsg.textContent = "";
+        }
+
+        if (textfield.value.length <= 3) {
+            cardCvc.textContent = textfield.value;
+        }
+
+        if (textfield.value.length === 0) {
+            cardCvc.textContent = "000";
+        }
+    }
+    
+}
+
+let errorFound = false;
 let originalCardNum = "0000 0000 0000 0000";
 textfields.forEach(textfield => {
-    textfield.addEventListener("input", (e) => {
+    textfield.addEventListener("input", (e) => console.log(errorCheckAndInsert(e, textfield)));
+});
 
 
-        if (textfield.id === "name" && textfield.value.length < 23) {
-            cardName.textContent = textfield.value.toUpperCase();
-
-            if (textfield.value.length === 0) {
-                cardName.textContent = "JANE APPLESEED";
-            }
-        } 
-            
-
-        // card number 
-        else if (textfield.id === "number") {
-
-            let errorText = displayError(textfield.value,textfield.value.length, numberErrorMsg.textContent, 20);
-            if (errorText != "") {
-                numberErrorMsg.textContent = errorText;
-                return;
-            } else {
-                numberErrorMsg.textContent = "";
-            }
 
 
-            if (textfield.value.length === 0) {
-                cardNum.textContent = "0000 0000 0000 0000"; // resets the card number
-            } 
-            else if (textfield.value.length === 20){ 
-                cardNum.textContent = textfield.value;
-            }
-            else {
-                cardNum.textContent = textfield.value;
-            }
-
-            if (e.inputType != "deleteContentBackward") {
-                if (textfield.value.length === 4 || textfield.value.length === 9 || textfield.value.length === 14) {
-                    textfield.value += " "; 
-                };
-            }
-        }
-
-        if (textfield.id === "month" || textfield.id === "year") {
-
-            let errorText = displayError(textfield.value,textfield.value.length, monthAndYearErrorMsg.textContent, 3);
-            if (errorText != "") {
-                monthAndYearErrorMsg.textContent = errorText;
-                return;
-            } else {
-                monthAndYearErrorMsg.textContent = "";
-            }
 
 
-            if (textfield.id === "month") {
-                if (textfield.value.length <= 2) {
-                    cardMonth.textContent = textfield.value;
-                }
-
-                if (textfield.value.length === 0) {
-                    cardMonth.textContent = "00";
-                }
-            }
-
-            if (textfield.id === "year") {
-                if (textfield.value.length <= 2) {
-                    cardYear.textContent = textfield.value;
-                } else {
-
-                }
-
-                if (textfield.value.length === 0) {
-                    cardYear.textContent = "00";
-                }
-            }
-        }
+// show complete state
+const formDetials = document.querySelector(".details");
+const completeState = document.querySelector(".complete-state");
 
 
-        if (textfield.id === "cvc") {
+const confirmBtn = document.querySelector(".confirm");
+const continueBtn = document.querySelector(".continue");
 
-            let errorText = displayError(textfield.value, textfield.value.length, cvcErrorMsg.textContent, 4);
-            if (errorText != "") {
-                cvcErrorMsg.textContent = errorText;
-                return;
-            } else {
-                cvcErrorMsg.textContent = "";
-            }
+confirmBtn.addEventListener("click", () => {
+    formDetials.style = "display: none;";
+    completeState.style = "display: flex;";
+});
 
-            if (textfield.value.length <= 3) {
-                cardCvc.textContent = textfield.value;
-            }
-
-            if (textfield.value.length === 0) {
-                cardCvc.textContent = "000";
-            }
-        }
-        
-    })
-})
+continueBtn.addEventListener("click", () => {
+    completeState.style = "display: none;";
+    formDetials.style = "display: flex;";
+});
